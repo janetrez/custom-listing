49dc647e92f222fb2aababd9c7efe36b20648c18
janetrez <treasaj@sahaj.ai>
2024-07-16 21:46:02 +0530

    Add Integer List extension functions

--- src/main/kotlin/IntegerListExtensions.kt
+++ src/main/kotlin/IntegerListExtensions.kt
@@ -20,3 +20,57 @@ fun BaseNodeList<Int>.getCube(): BaseNodeList<Int> {
     }
     return list
 }
+
+fun BaseNodeList<Int>.getOdd(): BaseNodeList<Int> {
+    val list = BaseNodeList<Int>()
+    var temp = this.head
+    while (temp != null) {
+        if(temp.value % 2 !=0)
+        list.add(temp.value)
+        temp = temp.next
+    }
+    return list
+}
+
+fun BaseNodeList<Int>.getEven(): BaseNodeList<Int> {
+    val list = BaseNodeList<Int>()
+    var temp = this.head
+    while (temp != null) {
+        if(temp.value % 2 ==0)
+            list.add(temp.value)
+        temp = temp.next
+    }
+    return list
+}
+
+fun BaseNodeList<Int>.getSum(): Int {
+    var sum = 0
+    var temp = this.head
+    while (temp != null) {
+        sum += temp.value
+        temp = temp.next
+    }
+    return sum
+}
+
+fun BaseNodeList<Int>.getMin(): Int {
+    var min = Int.MAX_VALUE
+    var temp = this.head
+    while (temp != null) {
+        if(temp.value < min)
+            min = temp.value
+        temp = temp.next
+    }
+    return min
+}
+
+fun BaseNodeList<Int>.getMax(): Int {
+    var max = Int.MIN_VALUE
+    var temp = this.head
+    while (temp != null) {
+        if(temp.value > max)
+            max = temp.value
+        temp = temp.next
+    }
+    return max
+}
--- src/test/kotlin/InterListExtensionTest.kt
+++ src/test/kotlin/InterListExtensionTest.kt
@@ -1,7 +1,6 @@
-import org.example.BaseNodeList
-import org.example.getCube
-import org.example.getSquare
+import org.example.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class InterListExtensionTest {
@@ -25,12 +24,78 @@ class InterListExtensionTest {
         val list1 = BaseNodeList<Int>()
         list1.add(1)
         list1.add(2)
-        val expectedSquare = BaseNodeList<Int>()
-        expectedSquare.add(1)
-        expectedSquare.add(8)
+        val expectedCube = BaseNodeList<Int>()
+        expectedCube.add(1)
+        expectedCube.add(8)
 
-        val square = list1.getCube()
+        val cube = list1.getCube()
 
-        assertTrue { square.equals(expectedSquare.getList()) }
+        assertTrue { cube.equals(expectedCube.getList()) }
     }
+
+    @Test
+    fun `should get the odd values of the list`() {
+        val list1 = BaseNodeList<Int>()
+        list1.add(1)
+        list1.add(2)
+        list1.add(3)
+        val expectedOdd = BaseNodeList<Int>()
+        expectedOdd.add(1)
+        expectedOdd.add(3)
+
+        val odd = list1.getOdd()
+
+        assertTrue { odd.equals(expectedOdd.getList()) }
+    }
+
+    @Test
+    fun `should get the even values of the list`() {
+        val list1 = BaseNodeList<Int>()
+        list1.add(1)
+        list1.add(2)
+        list1.add(3)
+        val expectedEven = BaseNodeList<Int>()
+        expectedEven.add(2)
+
+        val even = list1.getEven()
+
+        assertTrue { even.equals(expectedEven.getList()) }
+    }
+
+    @Test
+    fun `should get the sum of  values of the list`() {
+        val list1 = BaseNodeList<Int>()
+        list1.add(1)
+        list1.add(2)
+        list1.add(3)
+
+        val sum = list1.getSum()
+
+        assertEquals(6,sum)
+    }
+
+    @Test
+    fun `should get the minimum of  values of the list`() {
+        val list1 = BaseNodeList<Int>()
+        list1.add(4)
+        list1.add(2)
+        list1.add(3)
+
+        val minimum = list1.getMin()
+
+        assertEquals(2,minimum)
+    }
+
+    @Test
+    fun `should get the maximum of values of the list`() {
+        val list1 = BaseNodeList<Int>()
+        list1.add(4)
+        list1.add(2)
+        list1.add(3)
+
+        val maximum = list1.getMax()
+
+        assertEquals(4,maximum)
+    }
+
 }
